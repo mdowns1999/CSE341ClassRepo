8274a3431a73418c28b0a607f6c9d6e18605c8a3
mdowns <michaeljdowns99@gmail.com>
2023-01-14 07:19:48 -0700

    Added Body Parser

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use('/', require('./routes'))
+app.use(bodyParser.json())
+.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+}).use('/', require('./routes'));
+
 
 mongodb.initDb((err, mongodb) => {
     if (err) {
