99d80db1d395669c7d9b36dde947774f2286d98a
mdowns <michaeljdowns99@gmail.com>
2023-01-24 13:25:34 -0700

    Added Swagger

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/api-docs', require('./swagger'));
+
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
